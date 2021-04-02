d9733f830161bd35a7467777c92f91638611f4f4
Fagner Avila <fagner_avila99@hotmail.com>
2021-04-02 00:25:32 -0300

    first commit

---
+++ src/main/java/com/avila/pointInteligente/api/PontointeligenteApplication.java
@@ -0,0 +1,13 @@
+package com.avila.pointInteligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avila/pointInteligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avila.pointInteligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
